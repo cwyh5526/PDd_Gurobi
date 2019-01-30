3608ec04c846a498e18f06ec34187831af5f8631
Jisu <cwyh5526@gmail.com>
2019-01-31 00:17:29 +0900

    DefDef Face is fixed. the contacting vertex is not specified. so the constraints should not be ==. if you want to use previous one, you have to test all the vertex-Face pairs

--- Gurobi_Test2017/DefDefPD.cpp
+++ Gurobi_Test2017/DefDefPD.cpp
@@ -303,7 +303,7 @@ void DefDefPD::optDefDefFace(int fIndex, int pairIndex) {
 			model.addConstr(normal.x*t1[3].x + normal.y*t1[3].y + normal.z*t1[3].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c3");
 
 			//2.t2는 separating n 방향에 있다.						 
-			//model.addConstr(normal.x*t2[0].x + normal.y*t2[0].y + normal.z*t2[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c4");
+			model.addConstr(normal.x*t2[0].x + normal.y*t2[0].y + normal.z*t2[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c4");
 			model.addConstr(normal.x*t2[1].x + normal.y*t2[1].y + normal.z*t2[1].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c5");
 			model.addConstr(normal.x*t2[2].x + normal.y*t2[2].y + normal.z*t2[2].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c6");
 			model.addConstr(normal.x*t2[3].x + normal.y*t2[3].y + normal.z*t2[3].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c7");
@@ -313,12 +313,12 @@ void DefDefPD::optDefDefFace(int fIndex, int pairIndex) {
 			model.addConstr(normal.x*t1[1].x + normal.y*t1[1].y + normal.z*t1[1].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c9");
 			model.addConstr(normal.x*t1[2].x + normal.y*t1[2].y + normal.z*t1[2].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c10");
 
-			model.addConstr(normal.x*t2[0].x + normal.y*t2[0].y + normal.z*t2[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c11");
+			//model.addConstr(normal.x*t2[0].x + normal.y*t2[0].y + normal.z*t2[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c11");
 		}
 		else {
 
 			//1.t1은 separating normal 반대방향에 있다.
-			//model.addConstr(normal.x*t1[0].x + normal.y*t1[0].y + normal.z*t1[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c0");
+			model.addConstr(normal.x*t1[0].x + normal.y*t1[0].y + normal.z*t1[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c0");
 			model.addConstr(normal.x*t1[1].x + normal.y*t1[1].y + normal.z*t1[1].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c1");
 			model.addConstr(normal.x*t1[2].x + normal.y*t1[2].y + normal.z*t1[2].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c2");
 			model.addConstr(normal.x*t1[3].x + normal.y*t1[3].y + normal.z*t1[3].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) <= 0, "c3");
@@ -330,7 +330,7 @@ void DefDefPD::optDefDefFace(int fIndex, int pairIndex) {
 			model.addConstr(normal.x*t2[3].x + normal.y*t2[3].y + normal.z*t2[3].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) >= 0, "c7");
 
 			//3. mp는 separating plane 위에 있다.
-			model.addConstr(normal.x*t1[0].x + normal.y*t1[0].y + normal.z*t1[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0,  "c8");
+			//model.addConstr(normal.x*t1[0].x + normal.y*t1[0].y + normal.z*t1[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0,  "c8");
 																																		    
 			model.addConstr(normal.x*t2[0].x + normal.y*t2[0].y + normal.z*t2[0].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c9");
 			model.addConstr(normal.x*t2[1].x + normal.y*t2[1].y + normal.z*t2[1].z - (normal.x*mp.x + normal.y*mp.y + normal.z*mp.z) == 0, "c10");
